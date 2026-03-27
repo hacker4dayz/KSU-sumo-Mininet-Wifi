a0adc5206a73f621138c6179e034d3d2e8c23485
Leroy Malcolm <leroymalcolm68@gmail.com>
2026-03-27 12:51:52 -0400

    Update qos_controller.py

--- qos_controller.py
+++ qos_controller.py
@@ -9,13 +9,23 @@ from ryu.lib.packet import ether_types
 
 class QoSController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-
+    
     def __init__(self, *args, **kwargs):
         super(QoSController, self).__init__(*args, **kwargs)
+
+        # EMERGENCY VEHICLES (prio 300)
         self.high_priority_ips = [
             "10.0.0.213",  # Police IP
             "10.0.0.214"   # Ambulance IP
         ]
+
+        # BEST EFFORT NORMAL CARS (prio 100)
+        self.normal_car_ips = [
+        "10.0.0.201", "10.0.0.202", "10.0.0.203", "10.0.0.204", "10.0.0.205",
+        "10.0.0.206", "10.0.0.207", "10.0.0.208", "10.0.0.209", "10.0.0.210", 
+        "10.0.0.211", "10.0.0.212"
+        ]
+        
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
@@ -96,10 +106,7 @@ class QoSController(app_manager.RyuApp):
         # Emergency vehicle:
         if ip_pkt and (ip_pkt.dst in self.high_priority_ips or ip_pkt.src in self.high_priority_ips):
             priority = 300
-            self.logger.warning(
-                "Emergency vehicle flow src=%s dst=%s proto=%s in_port=%s",
-                ip_pkt.src, ip_pkt.dst, ip_pkt.proto, in_port
-            )
+            self.logger.warning("EMERGENCY %s -> %s prio=300", ip_pkt.src, ip_pkt.dst)
 
             match = parser.OFPMatch(
                 eth_type=ether_types.ETH_TYPE_IP,
@@ -115,10 +122,7 @@ class QoSController(app_manager.RyuApp):
         # High‑priority video multicast flow
         elif udp_pkt and ip_pkt.dst == "239.0.0.1" and udp_pkt.dst_port == 1234:
             priority = 200
-            self.logger.warning(
-                "Video flow detected src=%s dst=%s sport=%s dport=%s in_port=%s",
-                ip_pkt.src, ip_pkt.dst, udp_pkt.src_port, udp_pkt.dst_port, in_port
-            )
+            self.logger.warning("VIDEO %s -> 239.0.0.1:1234 prio=200", ip_pkt.src)
 
             match = parser.OFPMatch(
                 eth_type=ether_types.ETH_TYPE_IP,
@@ -130,32 +134,26 @@ class QoSController(app_manager.RyuApp):
             self.add_flow(datapath, 200, match, actions, msg.buffer_id)
 
         # Best‑effort UDP
-        elif udp_pkt:
-            priority = 10
-            self.logger.warning(
-                "Best effort UDP src=%s dst=%s sport=%s dport=%s in_port=%s",
-                ip_pkt.src, ip_pkt.dst, udp_pkt.src_port, udp_pkt.dst_port, in_port
-            )
+        elif ip_pkt and ip_pkt.src in self.normal_car_ips:
+            priority = 100
+            self.logger.warning("NORMAL %s -> %s prio=100", ip_pkt.src, ip_pkt.dst)
 
             match = parser.OFPMatch(
-                eth_type=0x0800,
-                ip_proto=17,
+                eth_type=ether_types.ETH_TYPE_IP,
                 ipv4_src=ip_pkt.src,
                 ipv4_dst=ip_pkt.dst,
-                udp_src=udp_pkt.src_port,
-                udp_dst=udp_pkt.dst_port
             )
+            if udp_pkt:
+                match.set_udp_src(udp_pkt.src_port)
+                match.set_udp_dst(udp_pkt.dst_port)
 
-            self.add_flow(datapath, 10, match, actions, msg.buffer_id)
+            self.add_flow(datapath, priority, match, actions, msg.buffer_id)
   
         else:
-            self.logger.warning(
-                "Best effort IPv4 src=%s dst=%s proto=%s in_port=%s",
-                ip_pkt.src, ip_pkt.dst, ip_pkt.proto, in_port
-            )
+            self.logger.warning("BEST EFFORT %s -> %s prio=5", ip_pkt.src, ip_pkt.dst)
 
             match = parser.OFPMatch(
-                eth_type=0x0800,
+                eth_type=ether_types.ETH_TYPE_IP,
                 ipv4_src=ip_pkt.src,
                 ipv4_dst=ip_pkt.dst,
                 ip_proto=ip_pkt.proto
@@ -171,3 +169,4 @@ class QoSController(app_manager.RyuApp):
             data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
         )
         datapath.send_msg(out)
+
